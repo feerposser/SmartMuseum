53f6b07dbf29deb9a569c2273a71ed44c7b3c486
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2020-12-16 17:16:06 -0300

    mudança do async getLocation do App

--- App.js
+++ App.js
@@ -14,15 +14,14 @@ export default class App extends React.Component {
     }
 
     this.onUID = this.onUID.bind(this)
-
-    this.getAccessCoarseLocation()
-    this.getAccessFineLocation()
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    await this.getAccessFineLocation()
+
     Eddystone.addListener("onUIDFrame", this.onUID)
-    console.log("monitorando...")
     Eddystone.startScanning()
+
     this.beaconMonitoring()
   }
 
@@ -31,7 +30,7 @@ export default class App extends React.Component {
     this.setState({ beacon: beacon.id })
   }
 
-  beaconMonitoring(refreshTime = 2000) {
+  beaconMonitoring(refreshTime = 500) {
     setInterval(() => {
       if (this.state.beacon) {
         this.setState({ data: this.state.beacon })
@@ -42,53 +41,31 @@ export default class App extends React.Component {
     }, refreshTime)
   }
 
-  async getAccessCoarseLocation() {
+  async getAccessFineLocation() {
     /*requisita acesso à localização do usuário 
       Android: também é necessário configurar o android/app/src/main/AndroidManifext.xml
       https://reactnative.dev/docs/permissionsandroid 
     */
     try {
       const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
         {
-          title: "ACCESS_COARSE_LOCATION",
-          message: "ACCESS_COARSE_LOCATION",
+          title: "ACCESS_FINE_LOCATION",
+          message: "ACCESS_FINE_LOCATION",
           buttonNeutral: "Ask Me Later",
           buttonNegative: "Cancel",
           buttonPositive: "OK"
         }
       );
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("OK -> ACCESS_COARSE_LOCATION");
+        console.log("OK -> ACCESS_FINE_LOCATION");
       } else {
-        console.log("NOT OK -> ACCESS_COARSE_LOCATION");
+        console.log("NOT OK -> ACCESS_FINE_LOCATION");
       }
     } catch (err) {
       console.warn(err);
     }
   }
-
-  async getAccessFineLocation() {
-    try {
-        const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-            {
-                title: "ACCESS_FINE_LOCATION",
-                message: "ACCESS_FINE_LOCATION",
-                buttonNeutral: "Ask Me Later",
-                buttonNegative: "Cancel",
-                buttonPositive: "OK"
-            }
-        );
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            console.log("OK -> ACCESS_FINE_LOCATION");
-        } else {
-            console.log("NOT OK -> ACCESS_FINE_LOCATION");
-        }
-    } catch (err) {
-        console.warn(err);
-    }
-}
 
   render() {
     return (
